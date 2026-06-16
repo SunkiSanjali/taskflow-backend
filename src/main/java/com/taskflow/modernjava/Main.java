cc5ced02093d385a10d3c5b4729d7a08bda254bf
Sunki Sanjali Reddy <sanjalireddysunki06@gmail.com>
2026-06-16 16:03:25 +0530

    feat: add switch pattern matching example

--- src/main/java/com/taskflow/modernjava/Main.java
+++ src/main/java/com/taskflow/modernjava/Main.java
@@ -1,14 +1,12 @@
 package com.taskflow.modernjava;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
+        // Text Block
         String message = """
                 Welcome to TaskFlow
                 Day 2 Modern Java
@@ -17,11 +15,13 @@ public class Main {
 
         System.out.println(message);
 
+        // Lambda Expression
         List<String> names = List.of("Sanjali", "Suraj", "Ashish");
 
         System.out.println("Names:");
         names.forEach(name -> System.out.println(name));
 
+        // Record Example
         List<Task> tasks = List.of(
                 new Task(1L, "Learn Java", "TODO"),
                 new Task(2L, "Learn Git", "DONE"),
@@ -29,42 +29,37 @@ public class Main {
                 new Task(4L, "Learn Spring", "DONE")
         );
 
+        // Stream Filter
         System.out.println("\nCompleted Tasks:");
-
         tasks.stream()
                 .filter(task -> task.status().equals("DONE"))
                 .forEach(System.out::println);
 
+        // Stream Count
         long count = tasks.stream()
                 .filter(task -> task.status().equals("DONE"))
                 .count();
 
-        System.out.println("\nCompleted Count: " + count);
+        System.out.println("\nCompleted Task Count: " + count);
 
-        System.out.println("\nSorted Tasks:");
-
-        tasks.stream()
-                .sorted(Comparator.comparing(Task::title))
-                .forEach(System.out::println);
-
-        System.out.println("\nTask Titles:");
-
-        tasks.stream()
-                .map(Task::title)
-                .forEach(System.out::println);
-
-        Map<String, List<Task>> grouped =
-                tasks.stream()
-                        .collect(Collectors.groupingBy(Task::status));
-
-        System.out.println("\nGrouped Tasks:");
-        System.out.println(grouped);
-
+        // Optional Example
         Optional<Task> foundTask = tasks.stream()
                 .filter(task -> task.id().equals(2L))
                 .findFirst();
 
         System.out.println("\nOptional Example:");
         foundTask.ifPresent(System.out::println);
+
+        // Sealed Interface + Switch Pattern Matching
+        TaskStatus status = new Todo();
+
+        String result = switch (status) {
+            case Todo t -> "Task is yet to start";
+            case InProgress i -> "Task is in progress";
+            case Done d -> "Task is completed";
+        };
+
+        System.out.println("\nSwitch Pattern Matching:");
+        System.out.println(result);
     }
 }
